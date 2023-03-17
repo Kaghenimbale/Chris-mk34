61da4b32419e5956bb5b6e620fb8f923d49690dd
Kagheni <Kaghenimbale@gmail.com>
2023-03-17 15:56:30 +0200

    Add code format

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import './About.css';
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <section id='about'>
-        <h2>Years of Experience</h2>
-        <p>My Resume</p>
+    <section id="about">
+      <h2>Years of Experience</h2>
+      <p>My Resume</p>
 
-        <div className="resume">
-          <div className="job">
-            <h3>Job Experience</h3>
-            <p>2010 - 2022</p>
+      <div className="resume">
+        <div className="job">
+          <h3>Job Experience</h3>
+          <p>2010 - 2022</p>
 
-            <div className="cards">
+          <div className="cards">
             <div className="card">
               <div className="card-1">
                 <h4>Electrical Engineer</h4>
                 <span>Prestech Sarl</span>
               </div>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo aliquam adipisci non distinctio! Earum alias quisquam sequi, tenetur repudiandae dignissimos?</p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo
+                aliquam adipisci non distinctio! Earum alias quisquam sequi,
+                tenetur repudiandae dignissimos?
+              </p>
             </div>
 
             <div className="card">
@@ -26,21 +30,29 @@ const About = () => {
                 <h4>Web Development</h4>
                 <span>Fidba Graphic</span>
               </div>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo aliquam adipisci non distinctio! Earum alias quisquam sequi, tenetur repudiandae dignissimos?</p>
-            </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo
+                aliquam adipisci non distinctio! Earum alias quisquam sequi,
+                tenetur repudiandae dignissimos?
+              </p>
             </div>
           </div>
-          <div className="education job">
+        </div>
+        <div className="education job">
           <h3>Education Experience</h3>
-            <p>2010 - 2022</p>
+          <p>2010 - 2022</p>
 
-            <div className="cards">
+          <div className="cards">
             <div className="card">
               <div className="card-1">
                 <h4>Bachelor in Network and Telecom(2021)</h4>
                 <span>International University of Equator</span>
               </div>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo aliquam adipisci non distinctio! Earum alias quisquam sequi, tenetur repudiandae dignissimos?</p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo
+                aliquam adipisci non distinctio! Earum alias quisquam sequi,
+                tenetur repudiandae dignissimos?
+              </p>
             </div>
 
             <div className="card">
@@ -48,13 +60,17 @@ const About = () => {
                 <h4>Bachelor in Electricity(2018)</h4>
                 <span>I.T.Kyeshero</span>
               </div>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo aliquam adipisci non distinctio! Earum alias quisquam sequi, tenetur repudiandae dignissimos?</p>
-            </div>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo
+                aliquam adipisci non distinctio! Earum alias quisquam sequi,
+                tenetur repudiandae dignissimos?
+              </p>
             </div>
           </div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,68 +1,101 @@
-import React from 'react'
-import './Contact.css';
-import {BsWhatsapp} from 'react-icons/bs';
-import {HiOutlineMail} from 'react-icons/hi';
-import {IoMdCheckmarkCircleOutline} from 'react-icons/io';
-import {BsFillSendFill} from 'react-icons/bs';
+import React from "react";
+import "./Contact.css";
+import { BsWhatsapp } from "react-icons/bs";
+import { HiOutlineMail } from "react-icons/hi";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { BsFillSendFill } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <section id='contact'>
-        <h2>CONTACT</h2>
-        <p>Contact With Me</p>
+    <section id="contact">
+      <h2>CONTACT</h2>
+      <p>Contact With Me</p>
 
-        <div className="contacts">
-            <div className="contact">
-                <div className="contact-card">
-                    <BsWhatsapp className='icon'/>
-                    <p>+243 977 813 248</p>
-                </div>
+      <div className="contacts">
+        <div className="contact">
+          <div className="contact-card">
+            <BsWhatsapp className="icon" />
+            <p>+243 977 813 248</p>
+          </div>
 
-                <div className="contact-card">
-                    <HiOutlineMail className='icon'/>
-                    <p>kaghenimbale@gmail.com</p>
-                </div>
+          <div className="contact-card">
+            <HiOutlineMail className="icon" />
+            <p>kaghenimbale@gmail.com</p>
+          </div>
 
-                <div className="contact-card">
-                    <IoMdCheckmarkCircleOutline className='icon'/>
-                    <p>Freelance available</p>
-                </div>
-            </div>
-
-            <div className="form-container">
-                <form action="#" method="post" id='form'>
-                    <div className="inputs">
-                    <label htmlFor="username">
-                        <input type="text" name="username" id="name" className='input' required placeholder='Enter Your Name'/>
-                    </label>
-
-                    <label htmlFor="usertel">
-                        <input type="tel" name="usertel" id="phone" className='input' required placeholder='Enter Your Phone'/>
-                    </label>
-
-                    <label htmlFor="email">
-                        <input type="email" name="username" id="name" className='input' required placeholder='Enter Your Email'/>
-                    </label>
-
-                    <label htmlFor="subject">
-                        <input type="" name="username" id="name" className='input' required placeholder='Enter Your Subject'/>
-                    </label>
-                    </div>
-                    <label htmlFor="message">
-                    <textarea name="message" id="message" className='input-message' placeholder='Enter Your Message'></textarea>
-                    </label>
-
-                    <div className="Submit-btn">
-                    <button type="submit" className='btn btn-submit'>
-                        <small>Send Your Message</small>
-                        <BsFillSendFill />
-                    </button>
-                    </div>
-                </form>
-            </div>
+          <div className="contact-card">
+            <IoMdCheckmarkCircleOutline className="icon" />
+            <p>Freelance available</p>
+          </div>
         </div>
+
+        <div className="form-container">
+          <form action="#" method="post" id="form">
+            <div className="inputs">
+              <label htmlFor="username">
+                <input
+                  type="text"
+                  name="username"
+                  id="name"
+                  className="input"
+                  required
+                  placeholder="Enter Your Name"
+                />
+              </label>
+
+              <label htmlFor="usertel">
+                <input
+                  type="tel"
+                  name="usertel"
+                  id="phone"
+                  className="input"
+                  required
+                  placeholder="Enter Your Phone"
+                />
+              </label>
+
+              <label htmlFor="email">
+                <input
+                  type="email"
+                  name="username"
+                  id="name"
+                  className="input"
+                  required
+                  placeholder="Enter Your Email"
+                />
+              </label>
+
+              <label htmlFor="subject">
+                <input
+                  type=""
+                  name="username"
+                  id="name"
+                  className="input"
+                  required
+                  placeholder="Enter Your Subject"
+                />
+              </label>
+            </div>
+            <label htmlFor="message">
+              <textarea
+                name="message"
+                id="message"
+                className="input-message"
+                placeholder="Enter Your Message"
+              ></textarea>
+            </label>
+
+            <div className="Submit-btn">
+              <button type="submit" className="btn btn-submit">
+                <small>Send Your Message</small>
+                <BsFillSendFill />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Contact;
