90e6b6088dad94b37d26c8fe9921d99eb4ec901d
Kagheni <Kaghenimbale@gmail.com>
2023-04-01 10:45:52 +0200

    Add styling to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,20 +16,6 @@ const Header = () => {
 
   return (
     <header id="header">
-      <div className={nav ? 'navLists' : ''}>
-        <ul className="nav-links">
-          <button className="close" onClick={renderNavBar}>
-            <AiOutlineClose className="closeIcon" />
-          </button>
-          {navList.map((list) => {
-            return (
-              <li className="navLink">
-                <a href="Chris">{list}</a>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
       <nav id="nav-bar">
         <div className="logo">
           <a href="#header">
@@ -37,74 +23,92 @@ const Header = () => {
           </a>
         </div>
 
-        <button className="btn" onClick={renderNavBar}>
+        <div className={nav ? 'navLists' : ''}>
+          <ul className="nav-links">
+            <button className="close" onClick={renderNavBar}>
+              <AiOutlineClose className="closeIcon" />
+            </button>
+            {navList.map((list) => {
+              return (
+                <li className="navLink">
+                  <a href="Chris">{list}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <button className="btn menuBtn" onClick={renderNavBar}>
           <a href="#cv">
             <CgMenuRightAlt className="icon-btn" />
           </a>
         </button>
       </nav>
 
-      <div className="descriptions">
-        <div className="description">
-          <h1>Kagheni Chris</h1>
-          <p>
-            Hello, my name is Kagheni Mbale Chris. I am a front-End developer
-            from the Republic Democratic of Congo. I can help you build a
-            product , feature or website Look through some of my work and
-            experience! If you like what you see and have a project you need
-            coded, don’t hestiate to contact me.
-          </p>
-          <div className="explore">
-            <a href="#me">Explore More</a>
-            <ImArrowRight2 className="icon" />
-          </div>
-
-          <div className="social">
-            <ul className="list-icons">
-              <a href="#https://github.com/Kaghenimbale">
-                <AiOutlineGithub className="icon" />
-              </a>
-              <a href="#https://www.linkedin.com/in/kagheni-mbale-401b90240/">
-                <BsLinkedin className="icon" />
-              </a>
-              <a href="#https://twitter.com/MbaleKagheni">
-                <BsTwitter className="icon" />
-              </a>
-              <a href="#https://www.instagram.com/kagheni_chris/">
-                <BsInstagram className="icon" />
-              </a>
-            </ul>
-          </div>
-          <a href="#cv" className="btn">
-            Download CV
-          </a>
-        </div>
-
-        <div className="experiences">
-          <div className="experience-soft">
-            <div className="experience">
-              <h2 className="myExperience">3+</h2>
-              <h2>Experience</h2>
-              <p>
-                I am Kagheni mbale Chris, I am a front End developer with 3
-                years of Experience
-              </p>
+      <div className="container">
+        <div className="descriptions">
+          <div className="description">
+            <h1>Kagheni Chris</h1>
+            <p>
+              Hello, my name is Kagheni Mbale Chris. I am a front-End developer
+              from the Republic Democratic of Congo. I can help you build a
+              product , feature or website Look through some of my work and
+              experience! If you like what you see and have a project you need
+              coded, don’t hestiate to contact me.
+            </p>
+            <div className="explore">
+              <a href="#me">Explore More</a>
+              <ImArrowRight2 className="icon" />
             </div>
-            <div className="experience">
-              <h2 className="myExperience">1k</h2>
-              <h2>Customers</h2>
-              <p>
-                I am Kagheni mbale Chris, I am a front End developer, I have
-                been working with thousands of customers
-              </p>
+
+            <div className="social">
+              <ul className="list-icons">
+                <a href="#https://github.com/Kaghenimbale">
+                  <AiOutlineGithub className="icon" />
+                </a>
+                <a href="#https://www.linkedin.com/in/kagheni-mbale-401b90240/">
+                  <BsLinkedin className="icon" />
+                </a>
+                <a href="#https://twitter.com/MbaleKagheni">
+                  <BsTwitter className="icon" />
+                </a>
+                <a href="#https://www.instagram.com/kagheni_chris/">
+                  <BsInstagram className="icon" />
+                </a>
+              </ul>
             </div>
+            <a href="#cv" className="btn">
+              Download CV
+            </a>
           </div>
 
-          <div className="explore">
-            <a href="#me">kaghenimbale@gmail.com</a>
-            <ImArrowRight2 className="icon" />
+          <div className="experiences">
+            <div className="experience-soft">
+              <div className="experience">
+                <h2 className="myExperience">3+</h2>
+                <h2>Experience</h2>
+                <p>
+                  I am Kagheni mbale Chris, I am a front End developer with 3
+                  years of Experience
+                </p>
+              </div>
+              <div className="experience">
+                <h2 className="myExperience">1k</h2>
+                <h2>Customers</h2>
+                <p>
+                  I am Kagheni mbale Chris, I am a front End developer, I have
+                  been working with thousands of customers
+                </p>
+              </div>
+            </div>
+
+            <div className="explore">
+              <a href="#me">kaghenimbale@gmail.com</a>
+              <ImArrowRight2 className="icon" />
+            </div>
           </div>
         </div>
+        <div className="profile">My pic</div>
       </div>
     </header>
   );
