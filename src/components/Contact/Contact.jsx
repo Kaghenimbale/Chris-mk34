6c4c94c8163299fb2838f2459179d46232536ae0
Kagheni <Kaghenimbale@gmail.com>
2023-04-15 14:17:43 +0200

    Refactor the Projects components

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Contact.css';
 import { BsWhatsapp } from 'react-icons/bs';
 import { HiOutlineMail } from 'react-icons/hi';
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,66 +1,89 @@
 import React from 'react';
 import './Projects.css';
-import img1 from '../../images/img1.jpg';
+import bookstore from '../../images/img1.png';
+import festival from '../../images/Festival.png';
+import jsportfolio from '../../images/JsPortfolio.png';
+import reacttodo from '../../images/ReactTodoList.png';
+import jstodo from '../../images/VanillaJsTodoList.png';
 
 const Projects = () => {
+  const projects = [
+    {
+      title: 'BookStore',
+      technologies: ['React', 'CSS', 'CRA'],
+      type: 'Web Application',
+      image: `${bookstore}`,
+      source: 'https://github.com/Kaghenimbale/bookstore-app.git',
+      liveDemo: 'https://visionary-daifuku-3ba202.netlify.app',
+    },
+    {
+      title: 'JS TodoList',
+      technologies: ['Html', 'CSS', 'JavaScript'],
+      image: `${jstodo}`,
+      type: 'Web Application',
+      source: 'https://github.com/Kaghenimbale/To-Do-List-app.git',
+      liveDemo: 'https://aesthetic-sunburst-db7cb6.netlify.app/',
+    },
+    {
+      title: 'React TodoList',
+      technologies: ['React', 'CSS', 'Vite'],
+      image: `${reacttodo}`,
+      type: 'Web Application',
+      source: 'https://github.com/Kaghenimbale/Todo-list-App.git',
+      liveDemo: 'https://gleeful-brigadeiros-299542.netlify.app/',
+    },
+    {
+      title: 'Amani Festival',
+      technologies: ['Html', 'CSS', 'JavaScript'],
+      image: `${festival}`,
+      type: 'Web Application',
+      source: 'https://github.com/Kaghenimbale/Capstone-project.git',
+      liveDemo: 'https://kaghenimbale.github.io/Capstone-project/',
+    },
+    {
+      title: 'Javascript Portfolio',
+      technologies: ['Html', 'CSS', 'JavaScript'],
+      image: `${jsportfolio}`,
+      type: 'Web Application',
+      source: 'https://github.com/Kaghenimbale/my-portfolio.git',
+      liveDemo: 'https://kaghenimbale.github.io/my-portfolio/',
+    },
+  ];
   return (
     <section id="projects">
       <h2>My Projects</h2>
       <p>Recents Works</p>
 
       <div className="myprojects">
-        <div className="project">
-          <div className="project-image">
-            <img src={img1} alt="Project1" />
-          </div>
-
-          <div className="project-description">
-            <div className="description-1">
-              <h3>National Events</h3>
-
-              <div className="languages">
-                <p>React</p>
-                <p>CSS</p>
+        {projects.map((item) => {
+          return (
+            <div className="project">
+              <div className="project-image">
+                <img src={item.image} alt="Project1" />
               </div>
-            </div>
-            <div className="description-2">
-              <div className="second">
-                <p>Web App</p>
-              </div>
-              <button className="btn" href="me">
-                <span>View More</span>
-              </button>
-            </div>
-          </div>
-        </div>
 
-        <div className="project">
-          <div className="project-image">
-            <img src={img1} alt="Project1" />
-          </div>
+              <div className="project-description">
+                <div className="description-1">
+                  <h3>{item.title}</h3>
 
-          <div>
-            <div className="project-description">
-              <div className="description-1">
-                <h3>Awesome Books</h3>
-
-                <div className="languages">
-                  <p>Html</p>
-                  <p>CSS</p>
-                  <p>Javascript</p>
+                  <div className="languages">
+                    <p>{item.technologies[0]}</p>
+                    <p>{item.technologies[1]}</p>
+                    <p>{item.technologies[2]}</p>
+                  </div>
+                </div>
+                <div className="description-2">
+                  <div className="second">
+                    <p>{item.type}</p>
+                  </div>
+                  <button className="btn" href="me">
+                    <span>View More</span>
+                  </button>
                 </div>
               </div>
-              <div className="description-2">
-                <div className="second">
-                  <p>Web App</p>
-                </div>
-                <button className="btn" href="me">
-                  <span>View More</span>
-                </button>
-              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </section>
   );
