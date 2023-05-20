92ac0a926ec91adaaa7ef4066016390cf0325c0a
Kagheni <Kaghenimbale@gmail.com>
2023-05-20 13:50:31 +0200

    Make some changes on the navigation bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -111,7 +111,7 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <div className="profile">My pic</div>
+        {/* <div className="profile">My pic</div> */}
       </div>
     </header>
   );
